c2addc8951694f5499d228a8ab8d5d6c32248179
FrostRabbit <68796320+FrostRabbit@users.noreply.github.com>
2026-06-28 20:59:49 +0800

    Update BPE tokenizer (Build GPT 1/10)

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -10,27 +10,15 @@ class Solution:
         #    c. Merge all non-overlapping occurrences left to right
         #    d. Record the merge as [token_a, token_b]
         # 3. Return the list of merges performed
-        chars = list(corpus)
+        chars = " ".join(list(corpus))
         results = []
         for _ in range(num_merges):
             if len(chars) < 2:
                 break
-        
-            count = Counter([(chars[i], chars[i+1]) for i in range(len(chars)-1)])
-            count = sorted(count.items(), key=lambda item: (-item[1], item[0]))
-            results.append(list(count[0][0]))
-            merge = ''.join(count[0][0])
-
-            i=0
-            temp = []
-
-            while i < len(chars):
-                if i < len(chars) - 1 and chars[i]+chars[i+1] == merge:
-                    temp.append(merge)
-                    i+=2
-                else:
-                    temp.append(chars[i])
-                    i+=1
-            
-            chars = temp
+            words = chars.split()
+            count = Counter([(words[i], words[i+1]) for i in range(len(words)-1)])
+            pair = sorted(count.items(), key=lambda item: (-item[1], item[0]))[0][0]
+            print(pair)
+            results.append(list(pair))
+            chars = chars.replace(' '.join(pair), ''.join(pair))
         return results
